9984804bfb9893d019f2d53bc2650a5563ac6f51
Cayden-B <Caydenbyrne18@gmail.com>
2023-12-13 16:46:12 +0000

    Added comments for my JS - added more validation

--- app.js
+++ app.js
@@ -1,3 +1,100 @@
+
+//test console.log for when I had issues with page connection.
+console.log("Test")
+
+
+let validateForm = (event) => {
+  event.preventDefault(); //prevent default behaviour
+  clearErrors();
+
+  //trim removing whitespace from both ends of the entries
+
+  let name = document.getElementById('name').value.trim(); 
+  let email = document.getElementById('email').value.trim();
+  let message = document.getElementById('message').value.trim();
+
+
+
+  //check if name is empty.
+  if (name === '') {
+    document.getElementById('nameError').innerText = 'Name is required';
+    return false;
+  }
+
+  //regex to check name has no numbers - only letters and spaces
+  let nameRegex = /^[A-Za-z\s]+$/;
+  if (!nameRegex.test(name)) {
+    document.getElementById("nameError").innerText = 'Name should contain only letters'
+    return false;
+  }
+
+  //email regex to check email submission has text > @ >. then text
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+  if (!emailRegex.test(email)) {
+    document.getElementById('emailError').innerText = 'Invalid email format';
+    return false;
+  }
+
+
+  //check if message is empty
+  if (message === '') {
+    document.getElementById('messageError').innerText = 'Message is required';
+    return false;
+  }
+
+  return true;
+  
+}
+
+
+//clear error messages associated with the 'nameError', 'emailError', 'messageError' fields
+function clearErrors() {
+  document.getElementById('nameError').innerText = '';
+  document.getElementById('emailError').innerText = '';
+  document.getElementById('messageError').innerText = '';
+}
+//clear entries associated with the 'name', 'email', 'message' fields
+function clearEntries(){
+  document.getElementById('name').value = "";
+  document.getElementById('email').value = "";
+  document.getElementById('message').value = "";
+}
+
+
+//check valid for submission  ↓
+
+function handleFormSubmission(event) {
+  if (!validateForm(event)) {
+    event.preventDefault();
+  } else {
+    alert("form has been submitted!! :)")
+    clearEntries()
+  }
+}
+
+// made this function for my buttons instead of typing it out one by one - previous code is in refactored section.
+let goUrl = (id, url) => {
+  document.getElementById(id).addEventListener("click", () => {
+    window.location.href = url;
+  })
+}
+
+goUrl("webpageRebuild", "https://clever-wescoff-fe42d4.netlify.app/")
+goUrl("Github", "https://github.com/Cayden-B")
+goUrl("bandPage", "https://cayden-b.github.io/Band-Page/")
+goUrl("Linkedin", "https://www.linkedin.com/in/cayden-byrne-297107227/")
+goUrl("land-page", "https://cayden-b.github.io/business-landing-page/")
+
+
+
+
+
+//Refactored code    ↓  ↓  ↓  ↓  ↓  ↓
+
+// document.getElementById("webpageRebuild").addEventListener("click", goProject1);
+// document.getElementById("bandPage").addEventListener("click", goBand);
+// document.getElementById("Linkedin").addEventListener("click", goLinkd);
+// document.getElementById("Github").addEventListener("click", goGithub);
 // function goLinkd() {
 //   window.open("https://www.linkedin.com/in/cayden-byrne-297107227/")
 // }
@@ -12,61 +109,3 @@
 // let goBand = () => {
 //   window.open("https://cayden-b.github.io/Band-Page/")
 // }
-
-console.log("Test")
-
-
-function validateForm(event) {
-  event.preventDefault();
-
-  clearErrors();
-
-  let name = document.getElementById('name').value.trim();
-  let email = document.getElementById('email').value.trim();
-  let message = document.getElementById('message').value.trim();
-
-  if (name === '') {
-    document.getElementById('nameError').innerText = 'Name is required';
-    return;
-  }
-
-  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    document.getElementById('emailError').innerText = 'Invalid email format';
-    return;
-  }
-
-  if (message === '') {
-    document.getElementById('messageError').innerText = 'Message is required';
-    return;
-  }
-
-  alert('Form submitted successfully!');
-}
-
-function clearErrors() {
-  document.getElementById('nameError').innerText = '';
-  document.getElementById('emailError').innerText = '';
-  document.getElementById('messageError').innerText = '';
-}
-
-let goUrl = (id, url) => {
-  document.getElementById(id).addEventListener("click", () => {
-    window.location.href = url
-  })
-}
-
-goUrl("webpageRebuild", "https://clever-wescoff-fe42d4.netlify.app/")
-goUrl("Github", "https://github.com/Cayden-B")
-goUrl("bandPage", "https://cayden-b.github.io/Band-Page/")
-goUrl("Linkedin", "https://www.linkedin.com/in/cayden-byrne-297107227/")
-goUrl("land-page", "https://cayden-b.github.io/business-landing-page/")
-
-
-
-
-
-// document.getElementById("webpageRebuild").addEventListener("click", goProject1);
-// document.getElementById("bandPage").addEventListener("click", goBand);
-// document.getElementById("Linkedin").addEventListener("click", goLinkd);
-// document.getElementById("Github").addEventListener("click", goGithub);
