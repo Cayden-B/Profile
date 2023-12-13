27312090767ed2e8e8309847808f58b410caead3
Cayden-B <Caydenbyrne18@gmail.com>
2023-12-13 15:40:40 +0000

    refactor of code for buttons

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-function goLinkd() {
-  window.open("https://www.linkedin.com/in/cayden-byrne-297107227/")
-}
-function goGithub() {
-  window.open("https://github.com/Cayden-B")
-}
+// function goLinkd() {
+//   window.open("https://www.linkedin.com/in/cayden-byrne-297107227/")
+// }
+// function goGithub() {
+//   window.open("https://github.com/Cayden-B")
+// }
 
-function goProject1() {
-  window.open("https://clever-wescoff-fe42d4.netlify.app/")
-}
+// function goProject1() {
+//   window.open("https://clever-wescoff-fe42d4.netlify.app/")
+// }
 
-let goBand = () => {
-  window.open("https://cayden-b.github.io/Band-Page/")
-}
+// let goBand = () => {
+//   window.open("https://cayden-b.github.io/Band-Page/")
+// }
 
 console.log("Test")
 
@@ -50,8 +50,23 @@ function clearErrors() {
   document.getElementById('messageError').innerText = '';
 }
 
+let goUrl = (id, url) => {
+  document.getElementById(id).addEventListener("click", () => {
+    window.location.href = url
+  })
+}
 
-document.getElementById("webpageRebuild").addEventListener("click", goProject1);
-document.getElementById("bandPage").addEventListener("click", goBand);
-document.getElementById("Linkedin").addEventListener("click", goLinkd);
-document.getElementById("Github").addEventListener("click", goGithub);
+goUrl("webpageRebuild", "https://clever-wescoff-fe42d4.netlify.app/")
+goUrl("Github", "https://github.com/Cayden-B")
+goUrl("bandPage", "https://cayden-b.github.io/Band-Page/")
+goUrl("Linkedin", "https://www.linkedin.com/in/cayden-byrne-297107227/")
+goUrl("land-page", "https://cayden-b.github.io/business-landing-page/")
+
+
+
+
+
+// document.getElementById("webpageRebuild").addEventListener("click", goProject1);
+// document.getElementById("bandPage").addEventListener("click", goBand);
+// document.getElementById("Linkedin").addEventListener("click", goLinkd);
+// document.getElementById("Github").addEventListener("click", goGithub);
